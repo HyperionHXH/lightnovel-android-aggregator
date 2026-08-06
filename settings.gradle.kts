591bfd7602e4db3afd9ed6d5d67ced16a9101e90
jiangyuyi <jiangyuyi@gmail.com>
2026-08-06 20:48:45 +0800

    Prefer official Maven repositories in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,39 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        if (System.getenv("CI")?.equals("true", ignoreCase = true) == true) {
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+            maven("https://maven.aliyun.com/repository/gradle-plugin")
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/central")
+        } else {
+            maven("https://maven.aliyun.com/repository/gradle-plugin")
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/central")
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
+        if (System.getenv("CI")?.equals("true", ignoreCase = true) == true) {
+            google()
+            mavenCentral()
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/central")
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/central")
+            google()
+            mavenCentral()
+        }
     }
 }
 
 rootProject.name = "LightNovel"
 include(":app")
-
